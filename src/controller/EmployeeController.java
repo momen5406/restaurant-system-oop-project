8b2afd97ebb9b748bf40e7d45e007bb9778944d7
motazahmed1 <106410512+motazahmed1@users.noreply.github.com>
2025-12-13 14:56:53 +0200

    Update EmployeeController.java

--- src/controller/EmployeeController.java
+++ src/controller/EmployeeController.java
@@ -1,13 +1,5 @@
 package controller;
 
 public class EmployeeController {
-    // TODO: MANAGE CUSTOMERS (ADD, DELETE, UPDATE, LIST, SEARCH)
-
-    // TODO: MAKE ORDER
-
-    // TODO: CANCEL ORDER
-
-    // TODO: BILLING
-
-    // TODO: SEND NOTIFICATION WHEN (SOMETHING HAPPEN)
+   
 }
