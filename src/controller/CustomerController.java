75ce2adf654d2a1bb9d6fc74510e7302970206a6
moner777 <moneramr19@gmail.com>
2025-12-17 23:13:27 +0200

    Update CustomerController.java

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -18,9 +18,7 @@ public class CustomerController {
         return null;
     }
 
-    // TODO: A method register for loyalty
 
-    // TODO: A method add order to history
 
     // TODO: A method to check if they have enough points to add a gift to thier profile
 }
